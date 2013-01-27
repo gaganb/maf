eab55ef720c60972525bdc632be47e1ec9d00e5f
GAGAN <gagan@gagan-laptop.(none)>
2013-01-27 13:56:55 +0530

    first c0mmit

---
+++ app/js/app-films-data.js
@@ -0,0 +1,92 @@
+MAF.Films = [
+	Ember.Object.create({
+		titles:
+		{
+			eng:'Najariya Ke Teer',
+			ind:'नजरिया के तीर'
+		},
+		producers:
+		{
+			eng:'Pappu Pardeshi',
+			ind:'पप्पू परदेशी'
+		},
+		directors:
+		{
+			eng:'Ram Awadh Patel',
+			ind:'राम अवध पटेल'
+		},
+		writers:
+		{
+			eng:'Dr. L. P. Baranwal',
+			ind:'डॉ एल पी बरनवाल'
+		},
+		starss:
+		{
+			eng:'Pappu Pardeshi',
+			ind:'पप्पू परदेशी'
+		},
+		date: '2012',
+		posterUrl: '../app/img/nkt_poster.jpg',
+		promoUrl: 'http://www.youtube.com/embed/2N1q9dgg60M'
+	}),
+	Ember.Object.create({
+		titles:
+		{
+			eng:'Vidhayak Jee',
+			ind:'विधायक जी'
+		},
+		producers:
+		{
+			eng:'Pappu Pardeshi',
+			ind:'पप्पू परदेशी'
+		},
+		directors:
+		{
+			eng:'Ram Awadh Patel',
+			ind:'राम अवध पटेल'
+		},
+		writers:
+		{
+			eng:'Dr. L. P. Baranwal',
+			ind:'डॉ एल पी बरनवाल'
+		},
+		starss:
+		{
+			eng:'Pappu Pardeshi',
+			ind:'पप्पू परदेशी'
+		},
+		date: '2012',
+		posterUrl: '../app/img/nkt_poster.jpg',
+		promoUrl: 'http://www.youtube.com/embed/2N1q9dgg60M'
+	}),
+	Ember.Object.create({
+		titles:
+		{
+			eng:'Dil Cheer Ke Dekhai',
+			ind:'दिल चीर के देखाई'
+		},
+		producers:
+		{
+			eng:'Pappu Pardeshi',
+			ind:'पप्पू परदेशी'
+		},
+		directors:
+		{
+			eng:'Ram Awadh Patel',
+			ind:'राम अवध पटेल'
+		},
+		writers:
+		{
+			eng:'Dr. L. P. Baranwal',
+			ind:'डॉ एल पी बरनवाल'
+		},
+		starss:
+		{
+			eng:'Pappu Pardeshi, Prachi Srivastava',
+			ind:'पप्पू परदेशी, प्राची श्रीवास्तवा'
+		},
+		date: '2010',
+		posterUrl: '../app/img/dckd_poster.png',
+		promoUrl: 'http://www.youtube.com/embed/2N1q9dgg60M'
+	})
+];
---
+++ app/js/app.js
@@ -0,0 +1,1 @@
+window.MAF = Em.Application.create();
